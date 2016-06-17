12ccd79cc3e8ca02dd25cd6e50b60d8ee362dfc9
suzp1984 <suzp1984@gmail.com>
2016-06-17 18:51:21 +0800

    upgrade build.gradle

--- app/src/main/java/echo/hybrid/suzp1984/github/io/echohybrid/MainActivity.java
+++ app/src/main/java/echo/hybrid/suzp1984/github/io/echohybrid/MainActivity.java
@@ -3,12 +3,16 @@ package echo.hybrid.suzp1984.github.io.echohybrid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.start_cordova)
+    Button startCordova;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
     }
 
     @Override
